418ce68656202f54ff6e2904cab02419378ef182
Chyoad <chyoadi123@gmail.com>
2022-11-09 14:39:41 +0700

    add AdiPage

--- app/src/main/java/com/example/dataku_app/adiPage.java
+++ app/src/main/java/com/example/dataku_app/adiPage.java
@@ -2,16 +2,13 @@ package com.example.dataku_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class adiatPage extends AppCompatActivity {
+public class adiPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_adiatpage);
+        setContentView(R.layout.activity_adipage);
     }
 }
