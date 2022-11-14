c2e499f5b10c548bf637a983aad9171e761acfae
Bagus Wicaksono <bagusbwicaksono5@student.uns.ac.id>
2022-11-14 19:51:35 +0700

    bagusPage

--- app/src/main/java/com/example/dataku_app/bagusPage.java
+++ app/src/main/java/com/example/dataku_app/bagusPage.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class adiatPage extends AppCompatActivity {
+public class bagusPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
